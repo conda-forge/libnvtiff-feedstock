6a82428da7dc9f1ae3fa33cd6074f49ad8de39e3
Daniel Ching <dching@nvidia.com>
2025-12-02 16:25:31 -0600

    BUG: Update build scripts for new source layout

--- recipe/build.sh
+++ recipe/build.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
 
 # Install to conda style directories
-[[ -d lib64 ]] && mv lib64 lib
+[[ -d lib/12 ]] && mv lib/12/* lib/ && rmdir lib/12
+[[ -d lib/13 ]] && mv lib/13/* lib/ && rmdir lib/13
+
 mkdir -p ${PREFIX}/lib
 [[ -d pkg-config ]] && mv pkg-config ${PREFIX}/lib/pkgconfig
 [[ -d "$PREFIX/lib/pkgconfig" ]] && sed -E -i "s|cudaroot=.+|cudaroot=$PREFIX|g" $PREFIX/lib/pkgconfig/nvtiff*.pc
